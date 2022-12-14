2e5a7711a4a2eaa1613d67a0ede99c2d60b4821c
OutbreakSource <daniel_ends@yahoo.com>
2022-12-14 14:23:29 -0700

    -new layout with scores, backdrops, etc..

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
 import './App.css';
 import {useState} from "react";
-import {Stack} from '@mui/material'
+import {Backdrop, Stack} from '@mui/material'
 
 function App() {
     const [rando, setRando] = useState(Math.floor(Math.random() * 10));
-    const [rando1, setRando1] = useState(1);
+    const [rando1, setRando1] = useState(Math.floor(Math.random() * 10));
     const [score, setScore] = useState(0)
-
-    function checkThis(){
-        if(test[rando].includes("trulySheeran")){
-            setScore(score + 1)
-            setRando(Math.floor(Math.random() * 10))
-            setRando1(Math.floor(Math.random() * 10))
-            console.log("check1")
-
-        }
-        else{
-            console.log(test[rando])
-            setRando(Math.floor(Math.random() * 10))
-            setRando1(Math.floor(Math.random() * 10))
-        }
-    }
-    function checkThat(){
-        if(test[rando1].includes("trulySheeran")){
-            setScore(score + 1)
-            setRando(Math.floor(Math.random() * 10))
-            setRando1(Math.floor(Math.random() * 10))
-            console.log("check1")
-
-        }
-        else{
-            setRando(Math.floor(Math.random() * 10))
-            setRando1(Math.floor(Math.random() * 10))
-        }
-    }
-    function checkBoth(){
-        if(!test[rando1].includes("trulySheeran") && !test[rando].includes("trulySheeran")){
-            setScore(score + 1)
-            setRando(Math.floor(Math.random() * 10))
-            setRando1(Math.floor(Math.random() * 10))
-        }
-        else{
-            setRando(Math.floor(Math.random() * 10))
-            setRando1(Math.floor(Math.random() * 10))
-        }
-
-    }
-
+    const [screen, setScreen] = useState(false)
+    const [maxScore, setMax] = useState(0);
     const test = [
         require('./Sheeran/1e839297489f85e77ebeff79f208f00c.jpg'),
         require('./Sheeran/51bed08814f4e33d2fef4638455246ae.jpg'),
@@ -61,21 +22,87 @@ function App() {
         require('./Sheeran/trulySheeran4.png'),
     ]
 
+    function checkThis(){
+        if(test[rando].includes("trulySheeran")){
+            setScore(score + 1)
+            getNewImage()
+        }
+        else{
+            setScreen(true)
+        }
+    }
+    function checkThat(){
+        if(test[rando1].includes("trulySheeran")){
+            setScore(score + 1)
+            getNewImage()
+
+        }
+        else{
+            setScreen(true)
+        }
+    }
+    function checkBoth(){
+        if(!test[rando1].includes("trulySheeran") && !test[rando].includes("trulySheeran")){
+            setScore(score + 1)
+            getNewImage()
+
+        }
+        else{
+            setScreen(true)
+
+        }
+
+    }
+
+    function reset(){
+        updateMax()
+        setScore(0)
+        setScreen(false)
+        getNewImage()
+    }
+
+    function updateMax(){
+        setMax(Math.max(score, maxScore))
+    }
+
+    function getNewImage(){
+        setRando(Math.floor(Math.random() * 10))
+        setRando1(Math.floor(Math.random() * 10))
+        if(rando === rando1){
+            setRando(Math.floor(Math.random() * 10))
+            setRando1(Math.floor(Math.random() * 10))
+        }
+    }
+
+
+
   return (
     <div className={"App"}>
-        <div className={"test"}>
-            <img src={test[rando]} alt={"test"} style={{height: 500, width: 500}}/>
+        <Backdrop open={screen}>
+            <Stack>
+                <text className={"blockText"}>
+                    Oops! Wrong ginger!
+                </text>
+                <button className={"button"} onClick={reset}>
+                    Reset?
+                </button>
+            </Stack>
+
+        </Backdrop>
+        <div className={"imageContainer"}>
+
+            <img src={test[rando]} alt={"test"} className={"image"}/>
             <Stack>
                 <button onClick={checkThis} className={"button"}>This</button>
                 <button onClick={checkThat} className={"button"}>That</button>
                 <button onClick={checkBoth} className={"button"}>Neither</button>
             </Stack>
 
-            <img src={test[rando1]} alt={"test"} style={{height: 500, width: 500}}/>
+            <img src={test[rando1]} alt={"test"} className={"image"}/>
         </div>
         <text style={{textAlign: "center", fontSize: 50}}>Score: {score}</text>
-
-
+        <br/>
+        <text style={{textAlign: "center", fontSize: 50}}>Max score: {maxScore}</text>
     </div>
 
 
